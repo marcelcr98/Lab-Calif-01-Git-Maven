13918c0cccb5edc4697b235da62c5ab65b6faae6
mcuellarr98 <mcuellarr98@gmail.com>
2019-09-03 17:32:05 -0500

    version 1

---
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.tecsup.lab01.controller;
+
+import com.tecsup.lab01.model.User;
+
+public class UserController {
+	
+	public static void main(String[] args) {
+		
+		User modelo = new User();
+		modelo.nombre();
+		modelo.apellido();
+		modelo.edad();
+	}
+
+}
---
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -0,0 +1,27 @@
+package com.tecsup.lab01.model;
+
+public class User {
+	
+	String nombre;
+    String apellido;
+	int edad;
+	
+	
+
+	public void nombre() {
+		
+		System.out.println("Marcelo");
+	}
+	
+public void apellido() {
+		
+		System.out.println("Cuellar");
+	}
+
+public void edad() {
+	
+	System.out.println("21");
+}
+	
+	
+}
